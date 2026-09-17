c00964c79c2c585d5ba996b5b627aa9cfee45260
Shrey Kumar Gupta <shreykr.gupta1260@gmail.com>
2026-09-17 11:22:19 +0530

    exp-2_added

---
+++ MERN_STACK/EXP-2/exp-2.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+
+fs.writeFileSync('student.txt' , 'B.Tech Node.js Lab');
+
+const data = fs.readFileSync('student.txt' , 'utf8');
+
+console.log(data);
+
+fs.writeFileSync('student.txt' , 'Name: Rahul\nSubject: Full Stack Development');
+
+console.log('File created successfully');
+
+fs.appendFileSync('studnet.txt' , '\nExperiment 2 completed.');
